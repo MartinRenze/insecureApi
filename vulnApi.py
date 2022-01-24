d91ed270a066a4d10e12fc2c8621205c4ab0439c
Martin Renze <github@martin-renze.de>
2022-01-24 14:25:05 +0100

    fix broken authentication

--- vulnApi.py
+++ vulnApi.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
+users = {
+    "admin": generate_password_hash("8s9dfnhsfd6uJ(2")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @app.route('/api/secret')
+@auth.login_required
 def secret():
     return 'Secret that no one can know: The admin can be easily bribed with schocolate cake!'
